44bcf93d2003ee5b49830a14d4f160b0243c5411
davidcortes241916 <alexis241916@gmail.com>
2026-02-09 23:14:06 -0500

    Se realizo el envio correo correctamente a correo, pero falta agtegar whatsapp y un buen tamaño, adicion se puede agregar una animacion de espera para el correo ya que tarda 2s en enviar y alistar el primer proyecto de suscripcion

--- apps/portafolio/urls.py
+++ apps/portafolio/urls.py
@@ -5,4 +5,10 @@ app_name= 'portafolio'
 
 urlpatterns = [
     path("", views.index, name="inicio"),
+    path("contacto/", views.contact, name="contacto"),
+    path("respuesta/", views.contact_ajax, name="contact_ajax"),
+    path("proyectos/", views.projects, name="proyectos"),
+    path("resumen/", views.resumen, name="resumen"),
+    #descargar CV
+    path('download-cv/', views.download_cv, name='download_cv'),
 ]
--- apps/portafolio/views.py
+++ apps/portafolio/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
-from django.contrib import messages
+from django.http import FileResponse, Http404
+import os
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import JsonResponse
+
 
 # Create your views here.
 def index(request):
     return render(request, "portafolio/index.html")
+
+def contact(request):
+    return render(request, "portafolio/contact.html")
+
+def contact_ajax(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        if not name or not email or not message:
+            return JsonResponse({
+                "success": False,
+                "error": "Todos los campos son obligatorios"
+            })
+
+        send_mail(
+            subject="Nuevo mensaje desde tu portafolio",
+            message=f"Nombre: {name}\nCorreo: {email}\n\nMensaje:\n{message}",
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=["davidalejandro241916@gmail.com"],
+        )
+
+        return JsonResponse({"success": True})
+
+    return JsonResponse({"success": False}, status=400)
+
+def projects(request):
+    return render(request, "portafolio/projects.html")
+
+def resumen(request):
+    return render(request, "portafolio/resumen.html")
+
+#descargar CV
+def download_cv(request):
+    file_path = os.path.join(
+        settings.BASE_DIR,
+        'static',
+        'docs',
+        'CV_Desarrollador_Backend_Junior_David_Cortes.pdf'
+    )
+
+    if not os.path.exists(file_path):
+        raise Http404("El archivo no existe")
+
+    return FileResponse(open(file_path, 'rb'), as_attachment=True)
