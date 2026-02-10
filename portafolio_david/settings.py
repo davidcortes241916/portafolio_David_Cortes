6093d5aa730bf606ecbb52be8053dcdd483d9a94
davidcortes241916 <alexis241916@gmail.com>
2026-02-09 23:14:06 -0500

    Se realizo el envio correo correctamente a correo, pero falta agtegar whatsapp y un buen tamaño, adicion se puede agregar una animacion de espera para el correo ya que tarda 2s en enviar y alistar el primer proyecto de suscripcion

--- portafolio_david/settings.py
+++ portafolio_david/settings.py
@@ -129,3 +129,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+#correo
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'davidalejandro241916@gmail.com'
+EMAIL_HOST_PASSWORD = 'leauhpaoxbanmuuj'
